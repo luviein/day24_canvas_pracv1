9f73a1731caf10b811f4d80e8fd6297437d152e6
Yenleng <yenlengggggg@gmail.com>
2023-07-19 21:09:39 +0800

    initial commit

---
+++ src/main/java/com/example/day24_canvas_pracv1/Day24CanvasPracv1Application.java
@@ -0,0 +1,13 @@
+package com.example.day24_canvas_pracv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day24CanvasPracv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day24CanvasPracv1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/day24_canvas_pracv1/Day24CanvasPracv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day24_canvas_pracv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day24CanvasPracv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
